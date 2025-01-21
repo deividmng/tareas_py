19ee9059ee9ad095f8505757fdaa8c1abf04850d
deividmng <mangassk8@gmail.com>
2025-01-21 08:43:34 +0000

    signin done and the superuser

--- tasks/admin.py
+++ tasks/admin.py
@@ -9,7 +9,7 @@ class TaskAdmin(admin.ModelAdmin):
 admin.site.register(Task, TaskAdmin)
 
 
-
+  
 # admin.ModelAdmin: Es la clase base correcta para personalizar el comportamiento de un modelo en el administrador de Django.
 # readonly_fields: Se utiliza para marcar campos como de solo lectura en el administrador. Asegúrate de que los nombres coincidan con los de tu modelo.
 # Registrar el modelo: admin.site.register(Task, TaskAdmin) registra el modelo Task con las configuraciones personalizadas de TaskAdmin.
